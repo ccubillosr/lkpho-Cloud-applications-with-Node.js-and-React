b0389c7cdd89ab7ecedb5e8915fc15ef46b6643e
ccubillosr <ccubillosr@hotmail.com>
2023-02-06 18:09:46 -0500

    React

---
+++ CD220Labs/react-apps/src/App-class-components.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const colorStyle = {
+    color:this.props.color,
+    fontSize:this.props.size+"px"
+    }
+    return (
+      <div>
+        <div style={colorStyle}>
+        Hello World!
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
